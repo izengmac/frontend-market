dde64698ae1d0e7d4d257267d825b3571bf61887
MarinaKononenko <macdanieltatenda00@gmail.com>
2024-04-25 01:34:43 +0300

    Managaer pages update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import LoginManagement from './pages_management/LoginManagement.js';
 import RecoverPasswordManagement from './pages_management/RecoverPasswordMangement';
 import PersonalDetailsManagement from './pages_management/PersonalDetailsManagement';
+import  ClientsOrders from './pages_management/ClientsOrders.js';
+import ClientsOrdersDetails from './pages_management/ClientsOrdersDetails.js';
+import ListSuppliers from './pages_management/ListSuppliers.js';
+
 
 function App() {
   return (
     <div className="App">
-      <PersonalDetailsManagement/>
+      <ListSuppliers/>
     </div>
   );
 }
